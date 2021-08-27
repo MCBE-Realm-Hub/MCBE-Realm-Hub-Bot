1fab618e7db016cefc529e432d93eab6dcc547ab
RomanMender <72093371+RomanMender@users.noreply.github.com>
2021-08-27 09:40:02 +0100

    Create logger.ts

---
+++ src/utils/logger.ts
@@ -0,0 +1,17 @@
+import chalk from 'chalk'
+import moment from 'moment'
+
+export default class Logger {
+  success(...content: unknown[]): void {
+    console.log(`${chalk.gray(moment().format('HH:mm:ss'))} ${chalk.green('[Success]')}`, ...content)
+  }
+  info(...content: unknown[]): void {
+    console.log(`${chalk.gray(moment().format('HH:mm:ss'))} ${chalk.cyan('[Info]')}`, ...content)
+  }
+  warn(...content: unknown[]): void {
+    console.log(`${chalk.gray(moment().format('HH:mm:ss'))} ${chalk.yellow('[Success]')}`, ...content)
+  }
+  error(...content: unknown[]): void {
+    console.log(`${chalk.gray(moment().format('HH:mm:ss'))} ${chalk.red('[Error]')}`, ...content)
+  }
+}
