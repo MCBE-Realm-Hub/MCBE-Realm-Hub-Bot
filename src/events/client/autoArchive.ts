63f063aeb4f64c0b5bdcd5f7abc2f561821ab633
notbeer <62919769+notbeer@users.noreply.github.com>
2021-12-22 22:13:15 -0500

    Update autoArchive.ts

--- src/events/client/autoArchive.ts
+++ src/events/client/autoArchive.ts
@@ -5,7 +5,7 @@ import config from '../../private/settings.json'
 export const event: Event = {
     name: 'threadUpdate',
     async execute(client, oldThread: ThreadChannel, newThread: ThreadChannel) {
-        if(newThread.archived && newThread.parentId == config.ID.threadChannel) {
+        if(newThread.archived && (newThread.parentId == config.ID.threadChannel || newThread.parentId == config.ID.buildsChannel)) {
             newThread.setArchived(false, 'Haha, no.')
         }
     }
