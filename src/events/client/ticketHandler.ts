7d14582e4d679731e421cf441e43f09bff646f86
Balloonieee <89361915+Balloonieee@users.noreply.github.com>
2022-01-16 16:35:15 -0500

    Update ticketHandler.ts

--- src/events/client/ticketHandler.ts
+++ src/events/client/ticketHandler.ts
@@ -1,1 +1,10 @@
+import { Event } from "../../@types/index";
+import { CommandInteraction } from 'discord.js';
 
+export const event: Event = {
+    name: 'interactionCreate',
+    async execute(client, interaction: CommandInteraction) {
+      if(!interaction.isButton()) return
+      console.log('nonce')
+    }
+};
