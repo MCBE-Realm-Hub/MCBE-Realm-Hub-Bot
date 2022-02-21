ae379ce68ba09018631c57093f7fa3e1c3760bc1
RomanMender <discojamesw@gmail.com>
2022-02-21 21:26:10 +0000

    Update onApplication.ts

--- src/events/server/onApplication.ts
+++ src/events/server/onApplication.ts
@@ -8,7 +8,7 @@ export const event: Event = {
     name: 'messageCreate',
     async execute(client, message: Message) {
         if (message.channel.id === channelID.application_channel) {
-            message.startThread({ name: `Moderator Application ${message.embeds[0].description.split('```')[1] || 'Unknown'}`, autoArchiveDuration: 'MAX' });
+            message.startThread({ name: `Moderator - ${message.embeds[0].description.split('```')[1] || 'Unknown'}`, autoArchiveDuration: 'MAX' });
         };
     }
 };
