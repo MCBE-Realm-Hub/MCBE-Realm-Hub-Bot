cd257110398df9ce048f438219d2431da698545e
RomanMender <discojamesw@gmail.com>
2022-02-21 12:00:06 +0000

    Fixed Intents

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const client = new Client({ 
+export const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
         Intents.FLAGS.DIRECT_MESSAGES,
         Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS
-    ], 
-    partials: ["CHANNEL"],
+    ],
+    partials: ["CHANNEL", "GUILD_MEMBER", "MESSAGE", "REACTION"],
     failIfNotExists: false
 });
 
