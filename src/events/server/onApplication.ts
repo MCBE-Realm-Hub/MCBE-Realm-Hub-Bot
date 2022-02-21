3826afd09d3b8b1cc2bb311a8d3dd86ad378e4ae
RomanMender <discojamesw@gmail.com>
2022-02-21 21:23:17 +0000

    Application smth

---
+++ src/events/server/onApplication.ts
@@ -0,0 +1,14 @@
+import { Message } from 'discord.js';
+
+import { channelID } from '../../../configuration/ID.json';
+
+import { Event } from "../../@types/index";
+
+export const event: Event = {
+    name: 'messageCreate',
+    async execute(client, message: Message) {
+        if (message.channel.id === channelID.application_channel) {
+            message.startThread({ name: `Moderator Application ${message.embeds[0].description.split('```')[1]}`, autoArchiveDuration: 'MAX' });
+        };
+    }
+};
