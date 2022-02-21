b1cc166c0d9fbeaf5b9739c97e550e47c2cacbd4
RomanMender <discojamesw@gmail.com>
2022-02-21 10:14:42 +0000

    Profanity blocketr

---
+++ src/events/server/onMessage.ts
@@ -0,0 +1,22 @@
+import {
+    Message,
+    MessageEmbed
+} from 'discord.js';
+
+import { accent } from '../../../assets/color/hex.json';
+import { Event } from "../../@types/index";
+import Filter from 'bad-words'
+
+var filter = new Filter();
+
+export const event: Event = {
+    name: 'messageCreate',
+    async execute(client, message: Message) {
+        if (filter.isProfane(message.content)) {
+            message.delete()
+            message.author.send({
+                content: 'Please refrain from using profanity in this server.'
+            }).catch((e) => message.channel.send({ content: 'Please refrain from using profanity in this server.' }))
+        }
+    }
+};
