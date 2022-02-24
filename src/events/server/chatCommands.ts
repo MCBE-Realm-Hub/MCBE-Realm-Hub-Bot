bd587cb2032520423787b517abc591d51c53febb
Dooka <96448741+DookaDess@users.noreply.github.com>
2022-02-23 21:14:05 -0800

    Updated chatcommands.js
    portfolio embed

--- src/events/server/chatCommands.ts
+++ src/events/server/chatCommands.ts
@@ -9,6 +9,7 @@ var embeds = [{"color":3092790,"image":{"url":"https://media.discordapp.net/atta
 //var embeds = [{ "color": 3092790, "image": { "url": "https://media.discordapp.net/attachments/945191743572279326/945207327387189279/RHinformation_2.png?width=1217&height=309" } }, { "title": ":wave:  **Welcome**!", "description": "*MCBE Realm Hub*, a __Minecraft Bedrock community, that specializes in Realm development__! As a community we discuss and help out with addons, packs, and gametest and many more topics. We have development channels made for collaboration and constructive feedback with a goal to spark inspiration, uniqueness, and creativity in realm development.", "color": 3092790, "image": { "url": "https://media.discordapp.net/attachments/945191743572279326/945198211436535818/Untitled.png?width=1217&height=26" } }, { "title": ":link:  **Links**", "description": "**Click on the linked buttons below to navigate through our main channels**. We ask you to read our **[Rules-Info](https://discordapp.com/channels/753438334663000116/945212679876919296)**! Also, check out our **[Portfolio](https://discordapp.com/channels/753438334663000116/945212679876919296)** where our community projects/packs are posted. Ask questions or get answers in our **[Help Desk](https://discordapp.com/channels/753438334663000116/945212679876919296)**.", "color": 3092790, "image": { "url": "https://media.discordapp.net/attachments/945191743572279326/945198211436535818/Untitled.png?width=1217&height=26" } }]
 var staffembed = [{ "color": 3092790, "image": { "url": "https://media.discordapp.net/attachments/819070111147229205/945387125568839680/RH_applications.png?width=1217&height=309" } }, { "title": "📝 **Staff Applications**!", "description": "__Become a part of the team today__! Fill out our *12 question Moderator application* now. Click the linked button below, and make sure to stay professional and use common sense when filling out your application. **NOTE** Some applications will not get accepted! Please fill out the application correctly.", "color": 3092790, "image": { "url": "https://media.discordapp.net/attachments/945191743572279326/945198211436535818/Untitled.png?width=1217&height=26" } }]
 //var embeds = [{"color":3092790,"image":{"url":"https://media.discordapp.net/attachments/945191743572279326/945207327387189279/RHinformation_2.png?width=1217&height=309"}},{"title":":wave:  **Welcome**!","description":"*MCBE Realm Hub*, a __Minecraft Bedrock community, that specializes in Realm development__! As a community we discuss and help out with addons, packs, and gametest and many more topics. We have development channels made for collaboration and constructive feedback with a goal to spark inspiration, uniqueness, and creativity in realm development.","color":3092790,"image":{"url":"https://media.discordapp.net/attachments/945191743572279326/945198211436535818/Untitled.png?width=1217&height=26"}},{"title":":link:  **Links**","description":"**Click on the linked buttons below to navigate through our main channels**. We ask you to read our **[Rules-Info](https://discordapp.com/channels/753438334663000116/945212679876919296)**! Also, check out our **[Portfolio](https://discordapp.com/channels/753438334663000116/945212679876919296)** where our community projects/packs are posted. Ask questions or get answers in our **[Help Desk](https://discordapp.com/channels/753438334663000116/945212679876919296)**.","color":3092790,"image":{"url":"https://media.discordapp.net/attachments/945191743572279326/945198211436535818/Untitled.png?width=1217&height=26"}}]
+var portembed = [{"color": 3092790,"image": {"url": "https://media.discordapp.net/attachments/945191743572279326/945809112879497246/Untitled9.png?width=1440&height=247"}},{"title": ":open_file_folder:  **Categories**","description": "Listed below are __main portfolio category buttons__, click on one your interested in finding a pack for. This will bring you to a thread with a list of projects for that type of development or project category.\n\n🔗 **Join these main categories threads so they get pinned under this channel**, or join the sub-thread projects in these threads.","color": 7720184,"image": {"url": "https://media.discordapp.net/attachments/945191743572279326/945198211436535818/Untitled.png?width=1440&height=31"}}]
 
 export const event: Event = {
     name: 'messageCreate',
@@ -38,10 +39,23 @@ export const event: Event = {
                         .setEmoji('945419732985712751')
                         .setURL('https://docs.google.com/forms/d/e/1FAIpQLSd5avDCOReL9IcgXctRi7IywJGrMv9ZpZ90ZDxm7Fz0O74fiw/viewform')
                         .setDisabled(false)
-                        //test
                 )
             message.channel.send({
                 embeds: staffembed,
+                components: [row]
+            })
+        }
+        if (message.content == '!sendportembed') {
+            var row = new MessageActionRow()
+                .addComponents(
+                    new MessageButton()
+                        .setLabel('Test')
+                        .setStyle('SUCCESS')
+                        .setEmoji('945392251486490644')
+                        .setCustomId('verify')
+                )
+            message.channel.send({
+                embeds: portembed,
                 components: [row]
             })
         }
