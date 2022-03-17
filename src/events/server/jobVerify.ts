8fa50395a1fb08c11e887e0b6afedf3df622052c
Dooka <96448741+DookaDess@users.noreply.github.com>
2022-03-17 01:28:48 -0700

    Update jobVerify.ts
    verify message

--- src/events/server/jobVerify.ts
+++ src/events/server/jobVerify.ts
@@ -13,14 +13,14 @@ export const event: Event = {
       interaction.guild.members.fetch(interaction.user.id).then(member => {
         if(member.roles.cache.has('953741470714716231'))
           return interaction.reply({
-            content: 'you have already verified for job/dev list!',
+            content: 'you have already verified to post job/dev list!',
             ephemeral: true
           })
         
         member.roles.add('953741470714716231')
         
         return interaction.reply({
-            content: 'you have been verified for job/dev list!',
+            content: 'you have been verified to post job/dev list!',
             ephemeral: true
           })
      })
